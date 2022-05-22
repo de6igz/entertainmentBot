35df7bd2058be73bf7730a76e6d785af213f9ec0
de6igz <Solopov2003@gmail.com>
2022-05-22 12:18:59 +0300

    added_books

--- bot.py
+++ bot.py
@@ -35,7 +35,16 @@ def main(message):
         show_top10_shooters(message)
     if message.text == 'Фильмы🎬':
         show_movies(message)
-
+    if message.text == 'Топ 10 Ужасов':
+        show_top10_horror_movies(message)
+    if message.text == 'Топ 10 Комедий':
+        show_top10_comedy_movies(message)
+    if message.text == 'Топ 10 Фантастики':
+        show_top10_fantastic_movies(message)
+    if message.text == 'Книги📚':
+        show_books(message)
+    if message.text=='Топ 10 кинг в жанре антиутопия':
+        show_top10_antiutopia_books(message)
 
 def show_games(message):
     games_categories = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -76,9 +85,58 @@ def show_top10_shooters(message):
 
 def show_movies(message):
     movies_categories = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    movies_categories.add('🔙Назад в главное меню', 'Топ 10 ужасов', 'Топ 10 Комедий', 'Топ 10 Фантастики',
+    movies_categories.add('🔙Назад в главное меню', 'Топ 10 Ужасов', 'Топ 10 Комедий', 'Топ 10 Фантастики',
                           row_width=1)
-    msg = bot.send_message(message.chat.id, '<i>Выбери жанр игр</i>', reply_markup=movies_categories, parse_mode='HTMl')
+    msg = bot.send_message(message.chat.id, '<i>Выбери жанр фильмов</i>', reply_markup=movies_categories,
+                           parse_mode='HTMl')
+
+
+def show_top10_horror_movies(message):
+    cursor.execute('select * from top10_horror_movies order by score desc')
+    movies_from_top10_horror = cursor.fetchall()
+    temp = []
+    for row in movies_from_top10_horror:
+        bot.send_message(message.chat.id,
+                         f'<i><b>Название</b></i>: {row[0]}\n<i><b>Длительность:</b></i> {row[1]}\n<i><b>Синопсис:</b></i> {row[2]}\n<i><b>Страна производства:</b></i> {row[3]}\n<i><b>Режиссёр:</b></i> {row[4]}\n<i><b>В ролях</b></i>: {row[5]}\n<i><b>Оценка на кинопоиске</b></i>: {row[6]}',
+                         parse_mode='HTML')
+
+
+def show_top10_comedy_movies(message):
+    cursor.execute('select * from top10_comedy_movies order by score desc')
+    movies_from_top10_comedy = cursor.fetchall()
+    temp = []
+    for row in movies_from_top10_comedy:
+        bot.send_message(message.chat.id,
+                         f'<i><b>Название</b></i>: {row[0]}\n<i><b>Длительность:</b></i> {row[1]}\n<i><b>Синопсис:</b></i> {row[2]}\n<i><b>Страна производства:</b></i> {row[3]}\n<i><b>Режиссёр:</b></i> {row[4]}\n<i><b>В ролях</b></i>: {row[5]}\n<i><b>Оценка на кинопоиске</b></i>: {row[6]}',
+                         parse_mode='HTML')
+
+
+def show_top10_fantastic_movies(message):
+    cursor.execute('select * from top10_fantastic_movies order by score desc')
+    movies_from_top10_fantastic = cursor.fetchall()
+    temp = []
+    for row in movies_from_top10_fantastic:
+        bot.send_message(message.chat.id,
+                         f'<i><b>Название</b></i>: {row[0]}\n<i><b>Длительность:</b></i> {row[1]}\n<i><b>Синопсис:</b></i> {row[2]}\n<i><b>Страна производства:</b></i> {row[3]}\n<i><b>Режиссёр:</b></i> {row[4]}\n<i><b>В ролях</b></i>: {row[5]}\n<i><b>Оценка на кинопоиске</b></i>: {row[6]}',
+                         parse_mode='HTML')
+
+
+def show_books(message):
+    books_categories = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    books_categories.add('🔙Назад в главное меню', 'Топ 10 кинг в жанре антиутопия', 'Топ 10 кинг в жанре психоделика',
+                         'Топ 10 книг в жанре гуманитарная фантастика',
+                         row_width=1)
+    msg = bot.send_message(message.chat.id, '<i>Выбери жанр книг</i>', reply_markup=books_categories, parse_mode='HTMl')
+
+
+def show_top10_antiutopia_books(message):
+    cursor.execute('select * from top10_antiutopia_books order by score desc')
+    top10_antiutopia_books = cursor.fetchall()
+    temp = []
+    for row in top10_antiutopia_books:
+        bot.send_message(message.chat.id,
+                         f'<i><b>Название</b></i>: {row[0]}\n<i><b>Автор:</b></i> {row[1]}\n<i><b>Синопсис:</b></i> {row[2]}\n<i><b>Оценка:</b></i> {row[3]}',
+                         parse_mode='HTML')
 
 
 bot.infinity_polling()
